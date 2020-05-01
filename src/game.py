88787bc4b39803ed7d7dcdfc1d57fd869b5ae956
Emil Tomczyk <emruemru1@gmail.com>
2020-05-01 17:27:17 +0200

    if'y jak są puste to muszą mieć pass

--- src/game.py
+++ src/game.py
@@ -17,18 +17,23 @@ def main():
             elif event.type ==pygame.KEYDOWN:
                 if event.key == pygame.K_w:
                     #move up
+                    pass
                 elif event.key == pygame.K_s:
                     #move down
+                    pass
                 elif event.key == pygame.K_a:
                     #move left
+                    pass
                 elif event.key == pygame.K_d:
                     #move right
+                    pass
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     #Left mouse button
+                    pass
                 elif event.button == 3:
-                    #right mouse button 
-
+                    #right mouse button
+                    pass
 
 if __name__ == "__main__":
     main()
