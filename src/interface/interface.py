92fa22bca9a321456c55604eb538b75be81af46b
NoMoreCrash <mm959@wp.pl>
2020-06-21 16:47:34 +0200

    Add files via upload

--- src/interface/interface.py
+++ src/interface/interface.py
@@ -47,23 +47,46 @@ class App:
         self.assets.convert()
         self.map_screen = MapSurface(self.map)
 
-    def test_screen(self):
-        """INITIALIZE STARTING SCREEN"""
-        self.screen.blit(self.map_screen.draw(7,7), (0, 0))
-        pygame.display.flip()
-    def run(self):
-        """Run the main event loop."""
-        """TEST"""
-        """TEST"""
-        while self.running:  
+
+
+    def button(self,msg,x,y,w,h,ic,ac,action=None):
+        mx, my = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x+w > mx > x and y+h > my > y:
+            pygame.draw.rect(self.screen, ac, (x,y,w,h))
+            if click[0] == 1 and action != None:
+                action()
+        else:
+            pygame.draw.rect(self.screen, ic, (x,y,w,h))
+        f = pygame.font.Font(None,16)
+        s = f.render(msg, True, [0,0,0])
+        self.screen.blit(s,(x+(w/2),y+(h/2)))
+
+
+    def intro(self):
+        self.running = True
+        while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
-                    raise SystemExit
+                    pygame.quit()
+                    quit()
+            self.screen.fill((240,255,255))
+            self.button("Play",200,100,50,50,(255,0,0),(0,255,0),self.game_loop)
+            self.button("Options",200,300,50,50,(255,0,0),(0,255,0))
+            self.button("Exit",200,500,50,50,(255,0,0),(0,255,0),quit)
+            pygame.display.update()
+
+    def game_loop(self):
+        while self.running:
+            self.screen.blit(self.map_screen.draw(7,7),(0,0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_w:
-                        self.test_screen()
                         #move up
+                        pass
                     elif event.key == pygame.K_s:
                         #move down
                         pass
@@ -80,6 +103,44 @@ class App:
                     elif event.button == 3:
                         #right mouse button
                         pass
+            pygame.display.flip() 
+
+
+    def test_screen(self):
+        """INITIALIZE STARTING SCREEN"""
+        self.screen.blit(self.map_screen.draw(7,7), (0, 0))
+
+    def run(self):
+        """Run the main event loop."""
+        """TEST"""
+        """TEST"""
+        while self.running:  
+            self.intro()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    raise SystemExit
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        #move up
+                        pass
+                    elif event.key == pygame.K_s:
+                        #move down
+                        pass
+                    elif event.key == pygame.K_a:
+                        #move left
+                        pass
+                    elif event.key == pygame.K_d:
+                        #move right
+                        pass
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        #Left mouse button
+                        pass
+                    elif event.button == 3:
+                        #right mouse button
+                        pass
+            pygame.display.flip()
 
         pygame.quit()
 
