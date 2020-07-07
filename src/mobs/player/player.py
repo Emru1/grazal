5a8f8c5c92c7889002d2b000795c28b9368c1990
Cvske <jakubbarr@gmail.com>
2020-07-07 13:24:21 +0200

    assety
    dodanie nowych assetow, animacja1

--- src/mobs/player/player.py
+++ src/mobs/player/player.py
@@ -4,6 +4,10 @@ from src.mobs.mob import Mob
 
 class Player(Mob):
 
+    up = 0
+    down = 0
+    left = 0
+    right = 0
     def __init__(self, x, y, mmap, hp, attack, movement, sprite):
         super().__init__(x, y, mmap, hp, attack, movement, sprite, name="Player")
         self.near_player = True
@@ -12,6 +16,8 @@ class Player(Mob):
     def moveup(self):
         if maps.get(self.mmap).get_tile(self.x, self.y - self.movement).passable:
             if not maps.get(self.mmap).get_tile(self.x, self.y - self.movement).mob:
+
+                self.asset = "gora"
                 maps.get(self.mmap).get_tile(self.x, self.y).mob = None
                 self.y = self.y - self.movement
                 maps.get(self.mmap).get_tile(self.x, self.y).mob = self
@@ -19,6 +25,7 @@ class Player(Mob):
     def movedown(self):
         if maps.get(self.mmap).get_tile(self.x, self.y + self.movement).passable:
             if not maps.get(self.mmap).get_tile(self.x, self.y + self.movement).mob:
+                self.asset = "ludek"
                 maps.get(self.mmap).get_tile(self.x, self.y).mob = None
                 self.y = self.y + self.movement
                 maps.get(self.mmap).get_tile(self.x, self.y).mob = self
@@ -26,6 +33,7 @@ class Player(Mob):
     def moveleft(self):
         if maps.get(self.mmap).get_tile(self.x - self.movement, self.y).passable:
             if not maps.get(self.mmap).get_tile(self.x - self.movement, self.y).mob:
+                self.asset = "lewo"
                 maps.get(self.mmap).get_tile(self.x, self.y).mob = None
                 self.x = self.x - self.movement
                 maps.get(self.mmap).get_tile(self.x, self.y).mob = self
@@ -33,6 +41,7 @@ class Player(Mob):
     def moveright(self):
         if maps.get(self.mmap).get_tile(self.x + self.movement, self.y).passable:
             if not maps.get(self.mmap).get_tile(self.x + self.movement, self.y).mob:
+                self.asset = "prawo"
                 maps.get(self.mmap).get_tile(self.x, self.y).mob = None
                 self.x = self.x + self.movement
                 maps.get(self.mmap).get_tile(self.x, self.y).mob = self
