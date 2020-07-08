10f3475e302f7cd06ea93ea7fc178a76b2531297
Emil Tomczyk <emruemru1@gmail.com>
2020-07-08 22:03:01 +0200

    refaktoryzacja

--- src/obj/obj.py
+++ src/obj/obj.py
@@ -1,39 +1,44 @@
 class Object:
-    def __init__(self, pos_x, pos_y, asset):
+    def __init__(self, pos_x, pos_y, path, asset):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.asset = asset
+        self.mmap = path
         self.can_eq = False
         self.can_mv = False
         self.on_ground = True
+        self.passable = True
 
     def pos(self):
-        return (self.pos_x, self.pos_y)
+        return self.pos_x, self.pos_y
 
 
 class Weapon(Object):
-    def __init__(self, pos_x, pos_y, asset, attack):
-        super().__init__(pos_x, pos_y, asset)
-        self.weapon = True
+    def __init__(self, pos_x, pos_y, path, asset, attack):
+        super().__init__(pos_x, pos_y, path, asset)
         self.attack_val = attack
+        self.can_eq = True
+        self.can_mv = True
 
 
 class Armor(Object):
-    def __init__(self, pos_x, pos_y, asset, armor):
-        super().__init__(pos_x, pos_y, asset)
-        self.armor = True
+    def __init__(self, pos_x, pos_y, path, asset, armor):
+        super().__init__(pos_x, pos_y, path, asset)
         self.armor = armor
+        self.can_eq = True
+        self.can_mv = True
 
 
 class Edible(Object):
-    def __init__(self, pos_x, pos_y, asset):
-        super().__init__(pos_x, pos_y, asset)
-        self.edible = True
+    def __init__(self, pos_x, pos_y, path, asset):
+        super().__init__(pos_x, pos_y, path, asset)
+        self.can_eq = True
+        self.can_mv = True
 
 
 class Potion(Edible):
-    def __init__(self, pos_x, pos_y, asset, hp_or_mp, points):
-        super().__init__(pos_x, pos_y, asset)
+    def __init__(self, pos_x, pos_y, path, asset, hp_or_mp, points):
+        super().__init__(pos_x, pos_y, path, asset)
         self.hp_or_mp = hp_or_mp
         self.points = points
 
